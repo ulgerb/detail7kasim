30beee33219fdd7abc61d7f07709062977a99631
Berkay Ülger <berkayulger45920@gmail.com>
2023-05-05 21:14:46 +0300

    -

--- appMy/models.py
+++ appMy/models.py
@@ -5,14 +5,21 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
    user = models.ForeignKey(User, verbose_name=("Kullanıcı Adı"), on_delete=models.CASCADE)
-   address = models.CharField(("Adres"), max_length=50, blank=True, default="-")
-   tel = models.CharField(("Telefon numarası"), max_length=10, blank=True, default="-")
-   job = models.CharField(("Meslek"), max_length=50, blank=True, default="-")
+   address = models.CharField(("Adres"), max_length=40, blank=True, default="-")
+   tel = models.CharField(("Telefon numarası"), max_length=10, default="-")
+   job = models.CharField(("Meslek"), max_length=20, blank=True, default="-")
    image = models.ImageField(("Profil Resmi"), upload_to='profile', max_length=255, blank=True, null=True)
    password = models.CharField(("Şifre"), max_length=50)
 
    def __str__(self):
       return self.user.username
+   
+   def save(self, *args, **kwargs):
+      self.tel = self.tel[:10]
+      self.job = self.job[:20]
+      self.address = self.address[:40]
+      super(Profile, self).save(*args, **kwargs)
+   
 class Brand(models.Model):
    title = models.CharField(("Marka"), max_length=50)
    
--- appMy/views.py
+++ appMy/views.py
@@ -126,7 +126,7 @@ def registerUser(request):
       password1 = request.POST.get("password1")
       password2 = request.POST.get("password2")
       
-      
+      # PASWORD START
       upp = False
       numm = False
       for i in password1:
@@ -142,6 +142,7 @@ def registerUser(request):
          messages.warning(request, "Şifrede bir numara olmalı!!")
       if not len(password1)>=6:
          messages.warning(request, "Şifrede en az 6 karakter olmalı!!")
+      # PASWORD END
 
       
       if fname.strip(" ") != "" and email.strip(" ") != "" and username.strip(" ") != "" and password1.strip(" ") != "":
@@ -184,6 +185,8 @@ def logoutUser(request):
 def profileUser(request):
    profile = Profile.objects.get(user = request.user)
    user = User.objects.get(username=request.user)
+   
+   
    if request.method == "POST":
       submit = request.POST.get("submit")
 
@@ -191,29 +194,75 @@ def profileUser(request):
          password = request.POST.get("password")
          password1 = request.POST.get("password1")
          password2 = request.POST.get("password2")
+
+         if user.check_password(password): # eski şifre
+            # PASWORD START
+            upp = False
+            numm = False
+            for i in password1:
+               print(i)
+               if i == i.upper():
+                  upp = True
+               if i.isdecimal():
+                  numm = True
+
+            if not upp:
+               messages.warning(request, "Şifrede bir büyük harf olmalı!!")
+            if not numm:
+               messages.warning(request, "Şifrede bir numara olmalı!!")
+            if not len(password1) >= 6:
+               messages.warning(request, "Şifrede en az 6 karakter olmalı!!")
+            # PASWORD END
+            if password1 == password2 and upp and numm and len(password1)>=6: # yeni şifreler
+               user.set_password(password1)
+               print(password1)
+               user.save()
+               # login(request, user)
+               return redirect("loginUser")
+            else:
+               messages.warning(request, "Şifreler aynı değil!!")
+         else:
+            messages.warning(request, "Şifre Yanlış!!")
+               
+         
          
       elif submit == "profilChange":
-         fullname = request.POST.get("fullname")
+         fullname = request.POST.get("fullname") # berkay ülger
          username = request.POST.get("username")
          job = request.POST.get("job")
          email = request.POST.get("email")
          address = request.POST.get("address")
          tel = request.POST.get("tel")
          image = request.FILES.get("image")
-         
+
          profile.job = job
          profile.address = address
          profile.tel = tel
          profile.image = image
          profile.save()
          
-         user.first_name = fullname
+         # NAME START
+         listname = fullname.split(" ")
+         fname = ""
+         lname = ""
+         if len(listname) != 1:
+            lname = listname.pop()
+         for i in listname:
+            if i != "":
+               fname += i + " "
+               
+         user.first_name = fname
+         user.last_name = lname
+         # NAME END
+         
          if not User.objects.filter(username = username).exists():
             user.username = username
             
          if not User.objects.filter(email = email).exists():
             user.email = email
          user.save()
+      
+      return redirect("profileUser")
    
    context = {
        "profile": profile,
